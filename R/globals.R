a9966ad9388e725c441a54b6b30cfc8834dfd7eb
PascalIrz <pascal.irz@gmail.com>
2021-01-12 07:51:07 +0100

    fix bugs creer_passerelle() + ajout de noms de variables et dataframes dans globals.R

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,4 @@
-utils::globalVariables(c('.', 'code_insee'))
+utils::globalVariables(c('.', 'code_insee', 'lot_poissons', 'lop_id', 'lop_pre_id',
+                         'prelevement_elementaire', 'pre_id', 'pre_op_id', 'operation',
+                         'ope_id', 'ope_pop_id', 'point_prelevement', 'pop_sta_id',
+                         'pop_id', 'sta_id'))
