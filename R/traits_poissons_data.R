99684adfa5cedb82090d8be1e9cd044bc370f454
PascalIrz <pascal.irz@gmail.com>
2021-04-11 18:57:10 +0200

    Ajout au package du jeu de données traits_poissons

---
+++ R/traits_poissons_data.R
@@ -0,0 +1,17 @@
+#' Données sommaires sur les espèces de poissons
+#'
+#' Tableau d'une centaine de lignes correspondant aux espèces de poissons
+#' référencées par leur code à trois lettres et 5 colonnes précisant le
+#' statut de ces espèces ainsi que leur principaux habitats.
+#'
+#' @docType data
+#'
+#' @usage data(traits_poissons)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' \donttest{
+#' data(traits_poissons)
+#' traits_poissons}
+"traits_poissons"
