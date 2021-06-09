7ae6d63ea0bac1db2f32e8cd00f63a48f45ea875
PascalIrz <pascal.irz@gmail.com>
2021-06-09 16:49:56 +0200

    résolu pb exemple imp_corres_aspe_taxref()

--- R/imp_corres_aspe_taxref.R
+++ R/imp_corres_aspe_taxref.R
@@ -6,20 +6,21 @@
 #' @param url Caractère. URL du jeu de données à télécharger.
 #'     Par défaut "https://api.sandre.eaufrance.fr/referentiels/v1/apt.csv?compress=true"
 #'
-#' @return Dataframe. Table de correspondance
+#' @return Dataframe. Table de correspondence.
 #' @export
 #'
 #' @importFrom readr read_csv2 locale
 #' @importFrom dplyr filter select
 #' @importFrom utils download.file
 #'
-#' #' @examples
+#' @examples
 #' \dontrun{
 #' taxref <- imp_corres_aspe_taxref()
 #' }
 imp_corres_aspe_taxref <- function(url = "https://api.sandre.eaufrance.fr/referentiels/v1/apt.csv?compress=true")
 
 {
+
   file <- paste0(tempdir(), "\\ref_sandre.csv.gz")
 
   download.file(url = url,
