0bb9ea2e1be4e106223156f808e2b9ad138c5de6
Cedric Mondy <cedric.mondy@gmail.com>
2021-06-17 11:55:02 +0200

    Ajoute la possibilité d'avoir des graphiques interactifs

--- R/gg_colo_ext_pops.R
+++ R/gg_colo_ext_pops.R
@@ -1,14 +1,18 @@
 #' Produire le graphique de la dynamique du peuplement pour chacun des points de prélèvement
 #'     du dataframe.
 #'
-#' @param df Le dataframe contenant les données, mises en forme par la fonction mef_colo_ext_pops().
+#' @param df Le dataframe contenant les données, mises en forme par la fonction \code{\link[=mef_colo_ext_pops]{mef_colo_ext()}}
+#' @param interactif Valeur logique définissant le type de graphique produit: statique (FALSE) ou interactif (TRUE)
+#' @param largeur,hauteur dimensions des graphiques interactifs
+#' @param ... arguments passés à la fonction \code{ggiraph::opts_sizing()}
 #'
 #' @return Une liste contenant les graphiques produits avec ggplot2.
 #' @export
 #'
-#' @importFrom ggplot2 ggplot aes geom_point labs scale_color_manual guides theme scale_x_continuous element_text element_blank
+#' @importFrom ggplot2 ggplot aes labs scale_color_manual guides theme scale_x_continuous element_text element_blank
+#' @importFrom ggiraph geom_point_interactive girafe opts_sizing
 #' @importFrom scales pretty_breaks
-#' @importFrom dplyr pull filter
+#' @importFrom dplyr pull filter rowwise ungroup
 #' @importFrom stats na.omit
 #' @importFrom purrr map
 #'
@@ -17,7 +21,7 @@
 #' mes_graphiques <- gg_colo_ext_pops(df = mon_df)
 #' mes_graphiques[[1]]
 #' }
-gg_colo_ext_pops <- function(df)
+gg_colo_ext_pops <- function(df, interactif = FALSE, largeur = 6, hauteur = 5, ...)
 
 {
   # based on https://stackoverflow.com/a/57086284
@@ -37,16 +41,26 @@ gg_colo_ext_pops <- function(df)
     }
   }
 
-  create_graph <- function(pop, df) {
+  create_graph <- function(pop, df, interactive) {
     df_pop <- df %>%
-      filter(pop_id == pop)
+      filter(pop_id == pop) %>%
+      rowwise() %>%
+      mutate(
+        hover = shiny::HTML(
+          paste0(
+            esp_code_alternatif, " (", annee, ")<br>",
+            "Effectif: ", effectif
+            )
+          )
+      ) %>%
+      ungroup()
 
     libelle <- df_pop %>%
       pull(pop_libelle) %>%
       na.omit() %>%
       .[1]
 
-    ggplot(
+    gg <- ggplot(
       data = df_pop,
       aes(
         x = annee,
@@ -56,7 +70,11 @@ gg_colo_ext_pops <- function(df)
         shape = type_point
       )
     ) +
-      geom_point() +
+      geom_point_interactive(
+        aes(
+          tooltip = hover
+        )
+        ) +
       labs(
         x = "",
         y = "",
@@ -83,13 +101,30 @@ gg_colo_ext_pops <- function(df)
         panel.background = element_blank(),
         panel.grid = element_blank()
       )
+
+    if (interactive) {
+      girafe(
+        ggobj = gg,
+        width_svg = largeur,
+        height_svg = hauteur,
+        options = list(
+          opts_sizing(...)
+        )
+        )
+    } else {
+      gg
+    }
   }
 
 
-  graphs <- map(
-    .x = df %>% pull(pop_id) %>% unique(),
-    .f = create_graph,
-    df = df) %>%
+  graphs <- df %>%
+    pull(pop_id) %>%
+    unique() %>%
+    map(
+      .f = create_graph,
+      df = df,
+      interactive = interactif
+      ) %>%
     set_names(unique(df$pop_id))
 
   if (length(graphs) == 1) {
