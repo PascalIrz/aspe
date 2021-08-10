094d9e5b279cfc013ffcc1f663ba591cab9cb005
PascalIrz <pascal.irz@gmail.com>
2021-08-10 16:02:58 +0200

    ajout mef_ajouter_operateur()

--- R/mef_creer_passerelle.R
+++ R/mef_creer_passerelle.R
@@ -36,7 +36,8 @@ mef_creer_passerelle <- function() {
     left_join(y = operation %>%
                 select(ope_id,
                        pop_id = ope_pop_id,
-                       pro_id = ope_pro_id)) %>%
+                       pro_id = ope_pro_id,
+                       poste_wama = ope_poste_wama)) %>%
     left_join(y = operation_objectif %>%
                 select(ope_id = opo_ope_id,
                        obj_id = opo_obj_id)) %>%
@@ -49,6 +50,7 @@ mef_creer_passerelle <- function() {
            ope_id,
            obj_id,
            pro_id,
+           poste_wama,
            pre_id,
            lop_id)
 }
