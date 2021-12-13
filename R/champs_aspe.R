4a33d93269ee6647edb339743a42568c785effc1
PascalIrz <pascal.irz@gmail.com>
2021-12-13 09:11:38 +0100

    ajout dataset champs_aspe

---
+++ R/champs_aspe.R
@@ -0,0 +1,13 @@
+#' Signification des champs de la base Aspe
+#'
+#' @docType data
+#'
+#' @usage data(champs_aspe)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' \donttest{
+#' data(champs_aspe)
+#' champs_aspe}
+"champs_aspe"
