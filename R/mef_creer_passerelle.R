b033d3859f7377769d2e8b2cf3ec3129cef745e7
PascalIrz <pascal.irz@gmail.com>
2022-01-26 14:16:09 +0100

    simplification mef_creer_passerelle()

--- R/mef_creer_passerelle.R
+++ R/mef_creer_passerelle.R
@@ -28,19 +28,21 @@
 #' }
 #'
 mef_creer_passerelle <- function() {
-  lot_poissons %>% select(lop_id,
+  lot_poissons %>%
+    select(lop_id,
                           pre_id = lop_pre_id) %>%
     left_join(y = prelevement_elementaire %>%
                 select(pre_id,
                        ope_id = pre_ope_id)) %>%
     left_join(y = operation %>%
                 select(ope_id,
-                       pop_id = ope_pop_id,
-                       pro_id = ope_pro_id,
-                       poste_wama = ope_poste_wama)) %>%
-    left_join(y = operation_objectif %>%
-                select(ope_id = opo_ope_id,
-                       obj_id = opo_obj_id)) %>%
+                       pop_id = ope_pop_id#,
+ #                      pro_id = ope_pro_id#,
+                       #poste_wama = ope_poste_wama
+                       )) %>%
+    # left_join(y = operation_objectif %>%
+    #             select(ope_id = opo_ope_id,
+    #                    obj_id = opo_obj_id)) %>%
     left_join(y = point_prelevement %>%
                 select (sta_id = pop_sta_id,
                         pop_id,
@@ -48,9 +50,9 @@ mef_creer_passerelle <- function() {
     select(sta_id,
            pop_id,
            ope_id,
-           obj_id,
-           pro_id,
-           poste_wama,
+           # obj_id,
+           # pro_id,
+        #   poste_wama,
            pre_id,
            lop_id)
 }
