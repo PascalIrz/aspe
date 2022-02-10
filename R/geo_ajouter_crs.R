a895dc8933705b73c40df16263a0fa2df2365635
PascalIrz <pascal.irz@gmail.com>
2022-02-10 11:39:00 +0100

    tidyversification geo_ajouter_crs() et geo_convertir_coords_df()

--- R/geo_ajouter_crs.R
+++ R/geo_ajouter_crs.R
@@ -8,19 +8,21 @@
 #' @return Le dataframe complété.
 #' @export
 #'
-#' @importFrom dplyr rename left_join
+#' @importFrom dplyr rename left_join enquo
 #'
 #' @examples
 #' \dontrun{
 #' pop <- point_prelevement %>%
-#' geo_ajouter_crs(var_id_crs = "pop_typ_id")
+#' geo_ajouter_crs(var_id_crs = pop_typ_id)
 #' }
 geo_ajouter_crs <- function(df, var_id_crs)
 
 {
 
+  var_id_crs <- enquo(var_id_crs)
+
   df %>%
-    rename(typ_id = var_id_crs) %>%
+    rename(typ_id = !!var_id_crs) %>%
     left_join(y = ref_type_projection %>%
                 select(typ_id,
                        typ_code_epsg))
--- R/geo_convertir_coords_df.R
+++ R/geo_convertir_coords_df.R
@@ -7,13 +7,13 @@
 #'
 #' @param df Dataframe contenant au moins trois colonnes pour les coordonnées et le
 #'     système de coordonnées (CRS)
-#' @param var_x Caractère. Nom de la colonne qui contient la longitude.
-#' @param var_y Caractère. Nom de la colonne qui contient la latitude.
-#' @param var_crs_initial Caractère. Nom de la colonne qui contient le numéro EPSG du CRS de chacune
+#' @param var_x Colonne qui contient la longitude.
+#' @param var_y Colonne qui contient la latitude.
+#' @param var_crs_initial Colonne qui contient le numéro EPSG du CRS de chacune
 #'     des observations (par exemple des stations ou des points).
 #' @param crs_sortie Numérique entier. Le numéro EPSG du CRS de sortie. Par défaut
 #'     c'est en WGS84 (crs = 4326).
-#' @param var_id Caractère. Nom de la colonne qui contient les identifiants des objets (=lignes) de df.
+#' @param var_id Colonne qui contient les identifiants des objets (=lignes) de df.
 #'
 #' @return Un dataframe à deux colonnes contenant les coordonnées dans le CRS de sortie.
 #'
@@ -30,12 +30,11 @@
 #'   left_join(y = ref_type_projection, by = c("sta_typ_id" = "typ_id"))
 #'
 #' coords_wgs84 <- geo_convertir_coords_df (df = data,
-#'     var_x = "sta_coordonnees_x",
-#'     var_y = "sta_coordonnees_y",
-#'     var_id = "sta_id",
-#'     var_crs_initial = "typ_code_epsg")
+#'     var_x = sta_coordonnees_x,
+#'     var_y = sta_coordonnees_y,
+#'     var_id = sta_id,
+#'     var_crs_initial = typ_code_epsg)
 #' }
-#'
 geo_convertir_coords_df <- function(df,
                                     var_x,
                                     var_y,
@@ -43,28 +42,38 @@ geo_convertir_coords_df <- function(df,
                                     var_crs_initial,
                                     crs_sortie = 4326)
 {
+  # gestion quotations
+  var_x <- deparse(substitute(var_x))
+  var_y <- deparse(substitute(var_y))
+  var_id <- enquo(var_id)
+  var_crs_initial <- enquo(var_crs_initial)
 
+  # vecteur des codes EPSG des CRS
   crss <- df %>%
-    pull(var_crs_initial) %>%
+    pull(!!var_crs_initial) %>%
     as.integer() %>%
     unique
 
+  # création du dataframe vide
   converted_coords <- data.frame()
 
-  for(crs in crss)
+  # alimentation du dataframe
+  # boucle sur les CRS. Pour chacun on effectue la conversion puis le tout est empilé
+  # pas réussi à le faire avec purrr::map() ou apply()
+  for (crs in crss)
 
   {
-
     sel_coords <- df %>%
       filter(typ_code_epsg == crs)
 
     coords <- sel_coords %>%
-      sf::st_as_sf(coords = c(var_x, var_y), crs = crs) %>%
-      sf::st_transform(crs = crs_sortie) %>%
-      sf::st_coordinates()
+      st_as_sf(coords = c(var_x, var_y),
+               crs = crs) %>%
+      st_transform(crs = crs_sortie) %>%
+      st_coordinates()
 
     ids <- sel_coords %>%
-      select(var_id)
+      select(!!var_id)
 
     sel_coords <- ids %>%
       cbind(coords)
