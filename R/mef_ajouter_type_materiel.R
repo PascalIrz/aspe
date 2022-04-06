50e02485b218496cbc30ffe71de84ecb82ce4ee6
PascalIrz <pascal.irz@gmail.com>
2022-04-06 08:30:16 +0200

    ajout mef_completer_type_materiel()

---
+++ R/mef_ajouter_type_materiel.R
@@ -0,0 +1,27 @@
+#' Ajouter à la passerelle le type de matériel employé
+#'     ex : Héron
+#'
+#' @param passerelle Dataframe "passerelle" mettant en correspondance les identifiants des différentes tables.
+#'
+#' @return La passerelle complétée.
+#' @export
+#'
+#' @importFrom dplyr left_join select
+#'
+#' @examples
+#' \dontrun{
+#' passerelle <- passerelle %>%
+#' mef_ajouter_type_materiel()
+#' }
+mef_ajouter_type_materiel <- function(passerelle)
+
+{
+  passerelle %>%
+    left_join(y = operation_description_peche %>%
+                select(ope_id = odp_ope_id,
+                       mom_id = odp_mom_id)) %>%
+    left_join(y = ref_modele_materiel %>%
+                select(mom_id,
+                       mom_libelle)) %>%
+    select(-mom_id)
+}
