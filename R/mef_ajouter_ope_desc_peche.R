4a7b66105d3b835f343fdff2da76dbc7cb5a35d5
PascalIrz <pascal.irz@gmail.com>
2023-04-21 09:30:11 +0200

    ajout fonction mef_ajouter_ope_desc_peche()

---
+++ R/mef_ajouter_ope_desc_peche.R
@@ -0,0 +1,36 @@
+#' Rajouter à la passerelle les variables de description de la pêche pour chaque opération (quand disponible).
+#'
+#' Ces variables sont issues de la table "operation_description_peche".
+#'
+#' @param passerelle Dataframe "passerelle" mettant en correspondance les identifiants des différentes tables.
+#'
+#' @return La passerelle à laquelle ont été ajoutés les variables environnementales.
+#' @export
+#'
+#' @importFrom dplyr left_join select mutate_all
+#'
+#' @examples
+#' \dontrun{
+#' passerelle <- mef_ajouter_ope_desc_peche(passerelle = passerelle)
+#' }
+mef_ajouter_ope_desc_peche <- function(passerelle)
+
+{
+
+  ope_dp <- passerelle %>%
+    left_join(    # jointure table operation_description_peche
+      y = operation_description_peche %>%
+        select(ope_id = odp_ope_id,
+               odp_duree_peche,
+               odp_longueur,
+               odp_largeur_lame_eau,
+               odp_temperature_instantanee,
+               odp_conductivite,
+               odp_tension:odp_nombre_epuisettes
+               ) %>%
+        mutate_all(function(x) ifelse(x == 0, NA, x)) # indeed zeros are missing values
+        )
+
+  ope_dp
+
+}
